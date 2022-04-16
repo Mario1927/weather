90a55d5d2cca2cc61720e37548e70f2a94904281
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-04-16 16:30:59 -0500

    New alert added for not founded cities

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -15,9 +15,21 @@ export default function SearchBar() {
     try {
       const geocoding = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${e.target[0].value}&limit=1&appid=${process.env.REACT_APP_WEATHER_API_KEY}`);
 
-      const lat: string = geocoding.data[0].lat;
-      const lon: string = geocoding.data[0].lon;
-      const name: string = geocoding.data[0].name;
+      const lat: string = geocoding.data[0]?.lat;
+      const lon: string = geocoding.data[0]?.lon;
+      const name: string = geocoding.data[0]?.name;
+
+      if(!lat || !lon || !name) {
+        Swal.fire({
+          title: 'Error',
+          text: 'City not found',
+          icon: 'error',
+          confirmButtonText: 'Ok',
+          background: 'rgb(15, 23, 42)',
+          color: 'rgb(241, 245, 249)'
+        });
+        return;
+      };
 
       if (listOfCities.includes(name)) {
         Swal.fire({
@@ -25,8 +37,8 @@ export default function SearchBar() {
           text: "You can't add the same city twice",
           icon: "warning",
           confirmButtonText: "Ok",
-          background: 'rgb(15 23 42)',
-          color: 'rgb(241 245 249)'
+          background: 'rgb(15, 23, 42)',
+          color: 'rgb(241, 245, 249)'
         });
         return;
       ;}
@@ -43,7 +55,7 @@ export default function SearchBar() {
   return (
     <div className="w-[300px] bg-slate-300 flex">
         <form className="flex-1 flex items-center justify-center" onSubmit={onSearch}>
-          <input className="w-full placeholder:text-slate-600 placeholder:text-center px-2 text-slate-900" placeholder='Search city...'></input>
+          <input className="w-full placeholder:text-slate-600 placeholder:text-center px-2 text-slate-900" placeholder='Search city...' autoFocus={true}></input>
           <button type="submit" className="w-[80px] text-slate-800 text-sm">Submit</button>
         </form>
     </div>

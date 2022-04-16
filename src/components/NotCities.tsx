67d28fadff60c1b596281dbb727746919124574d
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-04-16 15:00:09 -0500

    Fixed stylishing on NotCities component

--- src/components/NotCities.tsx
+++ src/components/NotCities.tsx
@@ -6,18 +6,18 @@ import Weather4 from '../assets/weather-4.gif';
 export default function NotCities() {
   return (
       <div className="w-full h-full flex flex-col justify-center items-center">
-        <div className="w-full h-full flex items-center justify-between my-4 border-2">
+        <div className="w-full h-[95vh] flex items-center justify-between">
             <div className='border-2 flex items-center justify-center w-full md:w-[50%] lg:w-[33%] xl:w-[25%]'>
-                <img src={Weather1} alt="weather" className="w-full h-[800px] block m-auto"/>
+                <img src={Weather1} alt="weather" className="w-full h-[90vh] block mx-auto"/>
             </div>
             <div className='hidden md:flex md:w-[50%] lg:w-[33%] xl:w-[25%] border-2'>
-                <img src={Weather2} alt="weather" className="w-full h-[800px] block m-auto"/>
+                <img src={Weather2} alt="weather" className="w-full h-[90vh] block mx-auto"/>
             </div>
             <div className='hidden lg:flex lg:w-[33%] xl:[25%] border-2'>
-                <img src={Weather3} alt="weather" className="w-full h-[800px] block m-auto"/>
+                <img src={Weather3} alt="weather" className="w-full h-[90vh] block mx-auto"/>
             </div>
             <div className='hidden xl:flex xl:w-[25%] border-2'>
-                <img src={Weather4} alt="weather" className="w-full h-[800px] block m-auto"/>
+                <img src={Weather4} alt="weather" className="w-full h-[90vh] block mx-auto"/>
             </div>
         </div>
         <div className='absolute w-[120px] top-[100px] right-[15px] text-slate-100 font-bold border-2 bg-slate-900 border-slate-900 px-1 '>

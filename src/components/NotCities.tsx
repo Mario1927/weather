8f18beb274ee1ebb201dc62b287adea97cd82fa8
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-04-16 14:48:41 -0500

    Fixed stylishing on NotCities component

--- src/components/NotCities.tsx
+++ src/components/NotCities.tsx
@@ -5,20 +5,22 @@ import Weather4 from '../assets/weather-4.gif';
 
 export default function NotCities() {
   return (
-    <div className="flex w-full h-full items-center justify-center my-4">
-        <div className='w-[400px] border-2'>
-            <img src={Weather1} alt="weather" className="w-[400px] h-[800px]"/>
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className="w-full h-full flex items-center justify-between my-4 border-2">
+            <div className='border-2 flex items-center justify-center w-full md:w-[50%] lg:w-[33%] xl:w-[25%]'>
+                <img src={Weather1} alt="weather" className="w-full h-[800px] block m-auto"/>
+            </div>
+            <div className='hidden md:flex md:w-[50%] lg:w-[33%] xl:w-[25%] border-2'>
+                <img src={Weather2} alt="weather" className="w-full h-[800px] block m-auto"/>
+            </div>
+            <div className='hidden lg:flex lg:w-[33%] xl:[25%] border-2'>
+                <img src={Weather3} alt="weather" className="w-full h-[800px] block m-auto"/>
+            </div>
+            <div className='hidden xl:flex xl:w-[25%] border-2'>
+                <img src={Weather4} alt="weather" className="w-full h-[800px] block m-auto"/>
+            </div>
         </div>
-        <div className='hidden w-[400px] md:flex border-2'>
-            <img src={Weather2} alt="weather" className="w-[400px] h-[800px]"/>
-        </div>
-        <div className='hidden w-[400px] lg:flex border-2'>
-            <img src={Weather3} alt="weather" className="w-[400px] h-[800px]"/>
-        </div>
-        <div className='hidden w-[400px] xl:flex border-2'>
-            <img src={Weather4} alt="weather" className="w-[400px] h-[800px]"/>
-        </div>
-        <div className='relative w-[120px] bottom-[-370px] right-[135px] text-slate-100 font-bold border-2 bg-slate-900 border-slate-900 px-1 '>
+        <div className='absolute w-[120px] top-[100px] right-[15px] text-slate-100 font-bold border-2 bg-slate-900 border-slate-900 px-1 '>
             <p>Search a city</p>
         </div>
     </div>
